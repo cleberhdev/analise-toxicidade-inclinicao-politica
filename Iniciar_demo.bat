d890305fb3c99b7e0086fe19d25fda563b63c4fa
cleberhdev <cleberhenriquelacerdaduarte@gmail.com>
2026-06-25 03:25:49 -0300

    atualizando arquvo bat

--- Iniciar_demo.bat
+++ Iniciar_demo.bat
@@ -11,7 +11,7 @@ if not exist ".venv\Scripts\python.exe" (
 echo Iniciando o servidor de modelos...
 start "Servidor de Modelos (NAO FECHE)" .venv\Scripts\python app.py
 timeout /t 8 /nobreak >nul
-start "" chrome "file:///C:/Users/Pedro Lucas/Desktop/Trabalho_Robson/site_projeto.html"
+start "" chrome "%~dp0site_projeto.html"
 echo.
 echo Pronto! Na pagina, role ate "Analisador". Deve mostrar "Modelo real conectado".
 echo Para encerrar, feche a janela do Servidor de Modelos.
